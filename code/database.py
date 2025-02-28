4151466eb1ff413ca69369d253aeb352733ec31b
yanis lamliji <yanis.lamliji@ynov.com>
2025-02-28 12:34:15 +0100

    first commit

---
+++ code/database.py
@@ -0,0 +1,6 @@
+import sqlite3 as sq
+
+conn=sq.connect("player.dbs")
+conn.cursor().execute("""create table if not exists player 
+(id integer primary key autoincrement, nom text)""")
+conn.close()
